e93dec68f7d6896b086cd3d492f774718986bf34
Vitoria Souza <munduko.plant@outlook.com>
2021-09-15 20:01:06 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var numeroSecreto = parseInt(Math.random() * 21);
+function chutar() {
+    var chute = parseInt(document.getElementById("valor").value);
+    var elementoResultado = document.getElementById("resultado");
+    if (chute == numeroSecreto) {
+        elementoResultado.innerHTML =
+            "<span>🎉Você acertou!🎉</span> <br><br>Atualize a página para adivinhar outro número.";
+    } else if (chute > 20 || chute < 0) {
+        elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10. 🤔";
+    } else if (chute > numeroSecreto) {
+        elementoResultado.innerHTML = "Bom palpite, pena que é maior do que deveria."
+    } else {
+        elementoResultado.innerHTML = "Um pouco mais.";
+    }
+}
+
+
+//Adicionar um número de tentativas para a pessoa tentar acertar e imprimir a resposta no final.
